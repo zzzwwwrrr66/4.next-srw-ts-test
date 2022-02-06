735f5c0ac8bf4d685c79b41418d0fb4695c5e382
wooram <zzzwwwrrr66@gmail.com>
2022-02-06 13:32:35 +0900

    <update> 0206 pagination

--- pages/api/useMovieDetail.ts
+++ pages/api/useMovieDetail.ts
@@ -31,7 +31,8 @@ export interface IMovieDetail  {
 export default function useMovieDetail(id:string){
   console.log(id);
   const fetcher = (url: string): Promise<IMovieDetail> => axios(url).then((res) => res.data).then(data=>data.data).then(data=> data.movie);
-  const { data, error, mutate } = useSWR(`https://yts.mx/api/v2/movie_details.json?movie_id=${id!}`, fetcher);
+  let URL = `https://yts.mx/api/v2/movie_details.json?movie_id=${id!}`;
+  const { data, error, mutate } = useSWR(URL, fetcher);
 
   return {
     data,
